827630179dc06cd4ecdc9904767a50431828ce40
Sam Wolters <sam@secretview.io>
2021-01-13 10:50:48 +0000

    Bug fixes

--- applicatie/public/wwwroot/components/navigation.php
+++ applicatie/public/wwwroot/components/navigation.php
@@ -1,4 +1,8 @@
-<?php session_start(); ?>
+<?php
+    if (!isset($_SESSION)) { 
+        session_start(); 
+    } 
+?>
 <ul>
     <li>
         <a class="navigation-logo" href="../../index.php">Fletnix</a>
